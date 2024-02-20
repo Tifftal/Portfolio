2dc3c9c0ecb03e2b48286309e5cb89abd1250ebc
VarTy <talankina.v@mail.ru>
2024-02-20 23:45:24 +0300

    added main application components

---
+++ portfolio/src/components/leftPannel/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const LeftPannel = () => {
+    return (
+        <div>
+            LEFT Pannel
+        </div>
+    )
+}
+
+export default LeftPannel;
---
+++ portfolio/src/components/rightPannel/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const RightPannel = () => {
+    return (
+        <div>
+            RIGHT Pannel
+        </div>
+    )
+}
+
+export default RightPannel;
