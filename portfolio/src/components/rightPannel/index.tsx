780958e62d2625db533414056e9f2be2d544aeb5
VarTy <talankina.v@mail.ru>
2024-02-23 17:35:36 +0300

    tech_stach, footer

--- portfolio/src/components/rightPannel/index.tsx
+++ portfolio/src/components/rightPannel/index.tsx
@@ -1,12 +1,16 @@
 import About from '../../widgets/about';
 import Projects from '../../widgets/projects';
 import './index.scss';
+import scroll from '../../assets/icons/mouse-cursor.png'
+import TechStack from '../../widgets/tech_stack';
 
 const RightPannel = () => {
     return (
         <div className='right-pannel'>
+            <div className='scroll'><img className="scroll-icon" src={scroll} /></div>
             <About />
-            <Projects/>
+            <Projects />
+            <TechStack />
         </div>
     )
 }
